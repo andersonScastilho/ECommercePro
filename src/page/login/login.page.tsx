bb83baa4c26b84efa845b0b2c8ac9330ef756fc4
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-10-07 09:21:07 -0300

    feat: add react Hook form to validation to Login component and Custom input Component

--- src/page/login/login.page.tsx
+++ src/page/login/login.page.tsx
@@ -1,6 +1,6 @@
 import { BsGoogle } from 'react-icons/bs'
 import { FiLogIn } from 'react-icons/fi'
-
+import { useForm } from 'react-hook-form'
 // Components
 import CustomButton from '../../components/custom-button/custom-button.component'
 import CustomInput from '../../components/custom-input/custom-input.component'
@@ -16,6 +16,14 @@ import {
 } from './login.page.styles'
 
 const LoginPage = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit
+  } = useForm()
+  const handleSubmitPress = (data: any) => {
+    console.log({ data })
+  }
   return (
     <>
       <Header />
@@ -28,13 +36,25 @@ const LoginPage = () => {
           <LoginSubtitle>Entre com seu e-mail</LoginSubtitle>
           <LoginInputContainer>
             <p>E-mail</p>
-            <CustomInput placeholder="Digite seu e-mail" />
+            <CustomInput
+              hasError={!!errors?.email}
+              placeholder="Digite seu e-mail"
+              {...register('email', { required: true })}
+            />
           </LoginInputContainer>
           <LoginInputContainer>
             <p>Senha</p>
-            <CustomInput placeholder="Digite sua senha" />
+            <CustomInput
+              hasError={!!errors?.password}
+              placeholder="Digite sua senha"
+              {...register('password', { required: true })}
+            />
           </LoginInputContainer>
-          <CustomButton startICon={<FiLogIn size={18} />}> Entrar</CustomButton>
+          <CustomButton
+            startICon={<FiLogIn size={18} />}
+            onClick={() => handleSubmit(handleSubmitPress)()}>
+            Entrar
+          </CustomButton>
         </LoginContent>
       </LoginContainer>
     </>
