b794da92cd72a4be25581e2133e521c20b76987c
Anderson Castilho <anderson.scastilho@hotmail.com>
2022-10-13 22:00:10 -0300

    feat: add loading component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { collection, getDocs, query, where } from 'firebase/firestore'
 import { FunctionComponent, useContext, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoadingComponent from './components/loading/loading.component'
 import { auth, db } from './config/firebase.config'
 import { UserContext } from './contexts/user.context'
 import { UserConverter } from './converters/firebase.converter'
@@ -37,7 +38,7 @@ const App: FunctionComponent = () => {
     setInitialize(false)
   })
 
-  if (isInitialize) return null
+  if (isInitialize) return <LoadingComponent />
 
   console.log(isAuthenticated)
   return (
