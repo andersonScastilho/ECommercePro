cb73b1461e94e53c70ffb46f7cf42ad9bb26067b
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-11-17 15:31:15 -0300

    style: add responsiveness to Cart  Component and Checkout Page

--- src/components/checkout/checkout.styles.ts
+++ src/components/checkout/checkout.styles.ts
@@ -14,6 +14,12 @@ export const CheckoutContainer = styled.div`
   button {
     width: 650px;
   }
+  @media (max-width: 768px) {
+    button {
+      width: 100%;
+    }
+    padding: 30px;
+  }
 `
 
 export const CheckoutTitle = styled.p`
@@ -42,6 +48,9 @@ export const CheckoutProducts = styled.div`
   ::-webkit-scrollbar-thumb:hover {
     background: ${Colors.text.dark};
   }
+  @media (max-width: 768px) {
+    min-width: 100%;
+  }
 `
 
 export const CheckoutTotal = styled.p`
@@ -49,4 +58,7 @@ export const CheckoutTotal = styled.p`
   font-size: 1.125rem;
   font-weight: 600;
   margin-bottom: 15px;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `
