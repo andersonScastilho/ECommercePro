cd12533bdc85b471abe9b58610c99e7a54a00cf2
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-01-02 19:42:19 -0300

    test: ensure input error message shows red error message

---
+++ src/components/input-error-message/input-error-message.spec.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react'
+import Colors from '../../theme/theme.colors'
+import InputErrorMessage from './input-error-message.component'
+
+describe('Input Error Message', () => {
+  it('Should show message with error color', () => {
+    const { getByText } = render(
+      <InputErrorMessage>Lorem Ipsum</InputErrorMessage>
+    )
+    const message = getByText('Lorem Ipsum')
+    expect(message).toHaveStyle({ color: `${Colors.error}` })
+  })
+})
