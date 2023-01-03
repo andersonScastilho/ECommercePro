de57e4bce57b24a001b44459b5ab6d6211ba6642
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-01-03 16:22:12 -0300

    feat: make preloaded on renderwhitredux opcional

--- src/helpeers/test.helper.tsx
+++ src/helpeers/test.helper.tsx
@@ -10,13 +10,13 @@ import { RootState } from '../store/store'
 export const renderWithRedux = (
   component: React.ReactElement,
   {
-    preloadedState,
+    preloadedState = {},
     store = configureStore({
       reducer: rootReducer,
       preloadedState
     }),
     ...renderOptions
-  }: { preloadedState: RootState; store?: any }
+  }: { preloadedState?: Partial<RootState>; store?: any }
 ) => {
   const Wrapper = ({ children }: { children: React.ReactElement }) => {
     return (
