7eb80eef8657462717057f79f359ae84c307d2f3
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-01-03 13:17:32 -0300

    test: ensure cart products count are correclty shown on header

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,4 +1,5 @@
 import { renderWithRedux } from '../../helpeers/test.helper'
+import CartProductType from '../../types/cart.types'
 import Header from './header.components'
 
 describe('Header', () => {
@@ -16,5 +17,31 @@ describe('Header', () => {
 
     getByText(/Login/i)
     getByText(/Criar Conta/i)
+  })
+  it('should show correct cart products count', () => {
+    const products: CartProductType[] = [
+      {
+        id: '1',
+        imageUrl: 'image_url',
+        name: 'Lorem Ipsum',
+        price: 5,
+        quantity: 10
+      },
+      {
+        id: '2',
+        imageUrl: 'image_url',
+        name: 'Lorem Ipsum2',
+        price: 5,
+        quantity: 7
+      }
+    ]
+    const { getByText } = renderWithRedux(<Header />, {
+      preloadedState: {
+        cartReducer: {
+          products
+        }
+      } as any
+    })
+    getByText(17)
   })
 })
