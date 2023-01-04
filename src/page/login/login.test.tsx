78c05c5a3078235543dfdfb2bd3d5447f392850c
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-01-04 17:21:14 -0300

    test: ensure login show error if email or password is invalid

--- src/page/login/login.test.tsx
+++ src/page/login/login.test.tsx
@@ -2,6 +2,11 @@ import userEvent from '@testing-library/user-event'
 import { renderWithRedux } from '../../helpeers/test.helper'
 import LoginPage from './login.page'
 
+import * as firebaseAuth from 'firebase/auth'
+import { AuthErrorCodes } from 'firebase/auth'
+
+jest.mock('firebase/auth')
+
 describe('Login', () => {
   it('should show error when trying to submit whitout filling all required fields', async () => {
     const { getByText, findByText } = renderWithRedux(<LoginPage />, {})
@@ -26,5 +31,45 @@ describe('Login', () => {
 
     userEvent.click(submitButton)
     await findByText(/E-mail invalido./i)
+  })
+  it('should show an error if email is not found', async () => {
+    const mockFirebaseAuth = firebaseAuth as any
+    mockFirebaseAuth.signInWithEmailAndPassword.mockImplementation(() =>
+      Promise.reject({ code: AuthErrorCodes.USER_DELETED })
+    )
+    const { getByPlaceholderText, findByText, getByText } = renderWithRedux(
+      <LoginPage />,
+      {}
+    )
+    const emailInput = getByPlaceholderText(/digite seu e-mail/i)
+    userEvent.type(emailInput, 'loremIpsum@gmail.com')
+
+    const passwordInput = getByPlaceholderText(/digite sua senha/i)
+    userEvent.type(passwordInput, '12345678')
+
+    const submitButton = getByText('Entrar')
+    userEvent.click(submitButton)
+
+    await findByText(/Este e-mail não esta cadastrado./i)
+  })
+  it('should show an error if password is not valid', async () => {
+    const mockFirebaseAuth = firebaseAuth as any
+    mockFirebaseAuth.signInWithEmailAndPassword.mockImplementation(() =>
+      Promise.reject({ code: AuthErrorCodes.INVALID_PASSWORD })
+    )
+    const { getByPlaceholderText, findByText, getByText } = renderWithRedux(
+      <LoginPage />,
+      {}
+    )
+    const emailInput = getByPlaceholderText(/digite seu e-mail/i)
+    userEvent.type(emailInput, 'loremIpsum@gmail.com')
+
+    const passwordInput = getByPlaceholderText(/digite sua senha/i)
+    userEvent.type(passwordInput, '1234')
+
+    const submitButton = getByText('Entrar')
+    userEvent.click(submitButton)
+
+    await findByText(/Senha Invalida/i)
   })
 })
