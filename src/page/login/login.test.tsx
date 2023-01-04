75c17f3e6a9327ee48eeb46229bfaf7cce78b052
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-01-04 17:06:18 -0300

    test: ensure login show if email is invalid

--- src/page/login/login.test.tsx
+++ src/page/login/login.test.tsx
@@ -13,4 +13,18 @@ describe('Login', () => {
     await findByText(/E-mail é obrigatório/i)
     getByText(/A senha é obrigatória/i)
   })
+  it('should show error is email is invalid', async () => {
+    const { getByPlaceholderText, findByText, getByText } = renderWithRedux(
+      <LoginPage />,
+      {}
+    )
+    const emailInput = getByPlaceholderText(/Digite seu e-mail/i)
+
+    userEvent.type(emailInput, 'invalid_email')
+
+    const submitButton = getByText('Entrar')
+
+    userEvent.click(submitButton)
+    await findByText(/E-mail invalido./i)
+  })
 })
